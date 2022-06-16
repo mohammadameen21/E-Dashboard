a49881f91acb57cddf1b3de3953b96bd60b959f9
Ameen <md.ameen1080@gmail.com>
2022-06-16 12:34:48 +0530

    Delete Api working perfectly

--- backend/index.js
+++ backend/index.js
@@ -55,4 +55,10 @@ app.get("/products", async  (req,resp) => {
     
 });
 
+
+app.delete("/product/:id", async (req, resp) => {
+    let result = await Product.deleteOne({_id: req.params.id});
+    resp.send(result)
+})
+
 app.listen(9800);
